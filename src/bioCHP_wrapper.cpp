a6dbc4322395fc8d883ec81449250523156d9c6c
gonzalodelalamo <gonzalodelalamo@gmail.com>
2025-04-02 09:32:14 +0200

    added modified wrapper

--- src/bioCHP_wrapper.cpp
+++ src/bioCHP_wrapper.cpp
@@ -7,7 +7,7 @@ extern "C" {
 bool bioCHP_plant_c(const char** fuel_def, int fuel_count, const double* Yj, int Yj_len,
                     const double* YH2Oj, int YH2Oj_len, double W_el, const double* Qk, int Qk_len, 
 		    const double* Tk_in, int Tk_in_len, const double* Tk_out, int Tk_out_len, double* Mj,
-                    int Mj_len, double* Q_prod, double W_el_prod, double* C_inv, double* C_op, double* C_op_var) {
+                    int Mj_len, double* Q_prod, double *W_el_prod, double* C_inv, double* C_op, double* C_op_var) {
 
   std::vector<std::string> fuel_vec;
   for (int i = 0; i < fuel_count; ++i) {
