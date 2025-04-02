005cabf84ec35296c0d509f93d56906e8de08ecd
gonzalodelalamo <gonzalodelalamo@gmail.com>
2025-04-02 09:19:33 +0200

    added modified bioCHP_wrapper.cpp

--- src/bioCHP_wrapper.cpp
+++ src/bioCHP_wrapper.cpp
@@ -7,7 +7,7 @@ extern "C" {
 bool bioCHP_plant_c(const char** fuel_def, int fuel_count, const double* Yj, int Yj_len,
                     const double* YH2Oj, int YH2Oj_len, double W_el, const double* Qk, int Qk_len, 
 		    const double* Tk_in, int Tk_in_len, const double* Tk_out, int Tk_out_len, double* Mj,
-                    int Mj_len, double* Q_prod, double W_el_prod, double* C_inv, double* C_op, double* C_op_avr) {
+                    int Mj_len, double* Q_prod, double W_el_prod, double* C_inv, double* C_op, double* C_op_var) {
 
   std::vector<std::string> fuel_vec;
   for (int i = 0; i < fuel_count; ++i) {
@@ -22,18 +22,18 @@ bool bioCHP_plant_c(const char** fuel_def, int fuel_count, const double* Yj, int
   std::vector<double> Mj_vec(Mj_len);
 
   bool result = bioCHP_plant(fuel_vec, Yj_vec, YH2Oj_vec, W_el, Qk_vec, Tk_in_vec,
-               Tk_out_vec, Mj_vec, Q_prod_val, W_el_prod_val, C_inv_val,
-               C_op_val, C_op_var_val);
+               Tk_out_vec, Mj_vec, Q_prod, W_el_prod, C_inv,
+               C_op, C_op_var);
 
   for (int i = 0; i < Mj_len; ++i) {
   Mj[i] = Mj_vec[i];
   }
 
-  *Q_prod = Q_prod_val;
-  *W_el_prod = W_el_prod_val;
-  *C_inv = C_inv_val;
-  *C_op = C_op_val;
-  *C_op_var = C_op_var_val;
+  *Q_prod = Q_prod;
+  *W_el_prod = W_el_prod;
+  *C_inv = C_inv;
+  *C_op = C_op;
+  *C_op_var = C_op_var;
 
   return result;
 }
