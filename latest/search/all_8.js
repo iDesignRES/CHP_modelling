34567ef4cbb503dd32a153ab507f576a78986e49
Zetison <Zetison@users.noreply.github.com>
2025-04-03 09:21:52 +0000

    deploy: 83fcfafee4b81a8fbd1161e634edbcb32c6b9740

---
+++ latest/search/all_8.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['nominal_20steady_20state_20operation_0',['Mass and energy flows (nominal steady state operation)',['../detailed_model_documentation.html#back-bio_CHP-math-balances',1,'']]],
+  ['nox_5freduction_5fparameters_1',['NOX_reduction_parameters',['../structNOX__reduction__parameters.html',1,'']]]
+];
---
+++ v0.1.0/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -0,0 +1,10 @@
+var dir_68267d1309a1af8e8297ef4c3efbcdba =
+[
+    [ "Libraries", "dir_a5e53cdfc4dc3e34218e25b9dcddbbe5.html", "dir_a5e53cdfc4dc3e34218e25b9dcddbbe5" ],
+    [ "bioCHP.cpp", "bioCHP_8cpp_source.html", null ],
+    [ "Cost.h", "Cost_8h_source.html", null ],
+    [ "Flows.h", "Flows_8h_source.html", null ],
+    [ "Parameters.cpp", "Parameters_8cpp_source.html", null ],
+    [ "pathfinder.h", "pathfinder_8h_source.html", null ],
+    [ "Processes.h", "Processes_8h_source.html", null ]
+];
