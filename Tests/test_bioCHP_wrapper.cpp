4a8828de17158a04b536bbd2038f59b5ee372f97
Jon Vegard Venås <jonvegard89@yahoo.com>
2025-04-07 09:35:03 +0200

    Fix formatting of test_bioCHP_wrapper.cpp

--- Tests/test_bioCHP_wrapper.cpp
+++ Tests/test_bioCHP_wrapper.cpp
@@ -7,15 +7,7 @@
 
 using namespace std;
 
-//extern "C" {
-//bool bioCHP_plant_c(const char** fuel_def, int fuel_count, const double* Yj, int Yj_len,
-//          const double* YH2Oj, int YH2Oj_len, double W_el, const double* Qk, int Qk_len, 
-//          const double* Tk_in, int Tk_in_len, const double* Tk_out, int Tk_out_len, double* Mj,
-//          int Mj_len, double* Q_prod, double *W_el_prod, double* C_inv, double* C_op, double* C_op_var);
-//}
-bool approxEqual(double a, double b, double tol=1e-4) {
-  return fabs(a - b) < tol;
-}
+bool approxEqual(double a, double b, double tol = 1e-4) { return fabs(a - b) < tol; }
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -29,26 +21,42 @@ int main(int argc, char* argv[]) {
   int fuel_count, Yj_len, YH2Oj_len, Qk_len, Tk_in_len, Tk_out_len, Mj_len;
   double Yj[4], YH2Oj[4], Qk[2], Tk_in[2], Tk_out[2], Mj[4];
   double W_el, Q_prod, W_el_prod, C_inv, C_op, C_op_var;
-  double expected_Mj[4], expected_Q_prod, expected_W_el_prod, expected_C_inv, expected_C_op, expected_C_op_var;
+  double expected_Mj[4], expected_Q_prod, expected_W_el_prod, expected_C_inv, expected_C_op,
+      expected_C_op_var;
 
-  switch(test) {
+  switch (test) {
     case 1:
-      fuel_def[0] = "spruce_stem"; fuel_def[1] = "spruce_bark"; fuel_def[2] = "birch_stem"; fuel_def[3] = "spruce_T&B";
+      fuel_def[0] = "spruce_stem";
+      fuel_def[1] = "spruce_bark";
+      fuel_def[2] = "birch_stem";
+      fuel_def[3] = "spruce_T&B";
       fuel_count = 4;
-      Yj[0] = 0.1; Yj[1] = 0.2; Yj[2] = 0.3; Yj[3] = 0.4;
+      Yj[0] = 0.1;
+      Yj[1] = 0.2;
+      Yj[2] = 0.3;
+      Yj[3] = 0.4;
       Yj_len = 4;
-      YH2Oj[0] = 0.4; YH2Oj[1] = 0.5; YH2Oj[2] = 0.35; YH2Oj[3] = 0.45;
+      YH2Oj[0] = 0.4;
+      YH2Oj[1] = 0.5;
+      YH2Oj[2] = 0.35;
+      YH2Oj[3] = 0.45;
       YH2Oj_len = 4;
       W_el = 100.0;
-      Qk[0] = 30.0; Qk[1] = 40.0;
+      Qk[0] = 30.0;
+      Qk[1] = 40.0;
       Qk_len = 2;
-      Tk_in[0] = 70.0; Tk_in[1] = 50.0;
+      Tk_in[0] = 70.0;
+      Tk_in[1] = 50.0;
       Tk_in_len = 2;
-      Tk_out[0] = 90.0; Tk_out[1] = 80.0;
+      Tk_out[0] = 90.0;
+      Tk_out[1] = 80.0;
       Tk_out_len = 2;
 
       Mj_len = 4;
-      expected_Mj[0] = 2.46386; expected_Mj[1] = 4.92771; expected_Mj[2] = 7.39157; expected_Mj[3] = 9.85543;
+      expected_Mj[0] = 2.46386;
+      expected_Mj[1] = 4.92771;
+      expected_Mj[2] = 7.39157;
+      expected_Mj[3] = 9.85543;
       expected_Q_prod = 70.0;
       expected_W_el_prod = 95.906862;
       expected_C_inv = 237.27238196773598133;
@@ -56,22 +64,37 @@ int main(int argc, char* argv[]) {
       expected_C_op_var = 14.54207213797799980;
       break;
     case 2:
-      fuel_def[0] = "spruce_stem"; fuel_def[1] = "birch_stem"; fuel_def[2] = "spruce_T&B"; fuel_def[3] = "spruce_bark";
+      fuel_def[0] = "spruce_stem";
+      fuel_def[1] = "birch_stem";
+      fuel_def[2] = "spruce_T&B";
+      fuel_def[3] = "spruce_bark";
       fuel_count = 4;
-      Yj[0] = 0.1; Yj[1] = 0.3; Yj[2] = 0.4; Yj[3] = 0.2;
+      Yj[0] = 0.1;
+      Yj[1] = 0.3;
+      Yj[2] = 0.4;
+      Yj[3] = 0.2;
       Yj_len = 4;
-      YH2Oj[0] = 0.4; YH2Oj[1] = 0.35; YH2Oj[2] = 0.45; YH2Oj[3] = 0.5;
+      YH2Oj[0] = 0.4;
+      YH2Oj[1] = 0.35;
+      YH2Oj[2] = 0.45;
+      YH2Oj[3] = 0.5;
       YH2Oj_len = 4;
       W_el = 100.0;
-      Qk[0] = 30.0; Qk[1] = 40.0;
+      Qk[0] = 30.0;
+      Qk[1] = 40.0;
       Qk_len = 2;
-      Tk_in[0] = 70.0; Tk_in[1] = 50.0;
+      Tk_in[0] = 70.0;
+      Tk_in[1] = 50.0;
       Tk_in_len = 2;
-      Tk_out[0] = 90.0; Tk_out[1] = 80.0;
+      Tk_out[0] = 90.0;
+      Tk_out[1] = 80.0;
       Tk_out_len = 2;
 
       Mj_len = 4;
-      expected_Mj[0] = 2.46386; expected_Mj[1] = 7.39157; expected_Mj[2] = 9.85543; expected_Mj[3] = 4.92771;
+      expected_Mj[0] = 2.46386;
+      expected_Mj[1] = 7.39157;
+      expected_Mj[2] = 9.85543;
+      expected_Mj[3] = 4.92771;
       expected_Q_prod = 70.0;
       expected_W_el_prod = 95.906862;
       expected_C_inv = 237.27238196773598133;
@@ -80,17 +103,17 @@ int main(int argc, char* argv[]) {
       break;
   }
 
-
-  if (bioCHP_plant_c(fuel_def, fuel_count, Yj, Yj_len, YH2Oj, YH2Oj_len, W_el, Qk, Qk_len, 
-           Tk_in, Tk_in_len, Tk_out, Tk_out_len, Mj, Mj_len, &Q_prod, &W_el_prod, &C_inv, &C_op, &C_op_var)) {
-
+  if (bioCHP_plant_c(fuel_def, fuel_count, Yj, Yj_len, YH2Oj, YH2Oj_len, W_el, Qk, Qk_len,
+                     Tk_in, Tk_in_len, Tk_out, Tk_out_len, Mj, Mj_len, &Q_prod, &W_el_prod,
+                     &C_inv, &C_op, &C_op_var)) {
     bool all_passed = true;
 
     for (int nj = 0; nj < Mj_len; nj++) {
       cout << "M_" << fuel_def[nj] << " = " << Mj[nj] << endl;
       if (!approxEqual(Mj[nj], expected_Mj[nj])) {
         all_passed = false;
-        cout << "Mj[" << nj << "] failed: expected " << expected_Mj[nj] << ", got " << Mj[nj] << endl;
+        cout << "Mj[" << nj << "] failed: expected " << expected_Mj[nj] << ", got "
+             << Mj[nj] << endl;
       }
     }
 
@@ -108,7 +131,8 @@ int main(int argc, char* argv[]) {
 
     if (!approxEqual(W_el_prod, expected_W_el_prod)) {
       all_passed = false;
-      cout << "W_el_prod failed: expected " << expected_W_el_prod << ", got " << W_el_prod << endl;
+      cout << "W_el_prod failed: expected " << expected_W_el_prod << ", got " << W_el_prod
+           << endl;
     }
 
     if (!approxEqual(C_inv, expected_C_inv)) {
@@ -123,7 +147,8 @@ int main(int argc, char* argv[]) {
 
     if (!approxEqual(C_op_var, expected_C_op_var)) {
       all_passed = false;
-      cout << "C_op_var failed: expected " << expected_C_op_var << ", got " << C_op_var << endl;
+      cout << "C_op_var failed: expected " << expected_C_op_var << ", got " << C_op_var
+           << endl;
     }
 
     if (all_passed) {
